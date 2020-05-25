e5657cc34c4df958100c0c964571dbbf83153c79
Somya-5353 <somyacarmel5353@gmail.com>
2020-05-25 22:35:03 +0530

    customising the navigation bar

--- TheBookSummary/ViewController.swift
+++ TheBookSummary/ViewController.swift
@@ -13,7 +13,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setUpNavigation()
+                                        
+   }
+    
+   //func to customize the navigation bar
+    func setUpNavigation() {
         
+        self.navigationController?.navigationBar.barTintColor = UIColor(red: 72/255.0, green: 61/255.0, blue: 139/255.0, alpha: 0.5)
+        self.navigationItem.title = "Book  Summary"
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white,
+                                                                         NSAttributedString.Key.font: UIFont(name: "Bradley Hand", size: 24)!,]
     }
 
 
