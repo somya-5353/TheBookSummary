4385efc6456fd6c3caa76d8fe98fdfa7b5f1b1a3
Somya-5353 <somyacarmel5353@gmail.com>
2020-05-30 00:41:56 +0530

    adding images to assets

--- TheBookSummary/Model/Genres.swift
+++ TheBookSummary/Model/Genres.swift
@@ -27,7 +27,7 @@ class Genres {
         genreList.append(Genres(genreName: "Fiction", displayImage: UIImage(named: "fiction1")!, displayColor: UIColor(red: 70/255.0, green: 130/255.0, blue: 230/255.0, alpha: 0.5)))
         genreList.append(Genres(genreName: "Classics", displayImage: UIImage(named: "selfHelp")!, displayColor: UIColor(red: 255/255.0, green: 99/255.0, blue: 71/255.0, alpha: 0.6)))
         genreList.append(Genres(genreName: "Self Help", displayImage: UIImage(named: "Biographies")!, displayColor: UIColor(red: 107/255.0, green: 142/255.0, blue: 35/255.0, alpha: 0.6)))
-        genreList.append(Genres(genreName: "Leadership", displayImage: UIImage(named: "fiction")!, displayColor: UIColor(red: 147/255.0, green: 112/255.0, blue: 219/255.0, alpha: 0.3)))
+        genreList.append(Genres(genreName: "Thrillers", displayImage: UIImage(named: "fictions")!, displayColor: UIColor(red: 147/255.0, green: 112/255.0, blue: 219/255.0, alpha: 0.3)))
         genreList.append(Genres(genreName: "Biographies", displayImage: UIImage(named: "book5")!, displayColor: UIColor(red: 210/255.0, green: 105/255.0, blue: 30/255.0, alpha: 0.5)))
         
         return genreList
