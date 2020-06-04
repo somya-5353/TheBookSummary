b41f0fbb629c02ecb76789243034a5870ee74c63
Somya-5353 <somyacarmel5353@gmail.com>
2020-06-04 22:55:33 +0530

    restructuring the project folders

---
+++ TheBookSummary/ViewControllers/SummaryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SummaryViewController.swift
+//  TheBookSummary
+//
+//  Created by Somya on 04/06/20.
+//  Copyright © 2020 Somya. All rights reserved.
+//
+
+import UIKit
+
+class SummaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
