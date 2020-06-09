467b4e0efa323ae8c140992f86a7a198af1e08d5
Somya-5353 <somyacarmel5353@gmail.com>
2020-06-09 19:06:32 +0530

    making the constants file

---
+++ TheBookSummary/Utils/ConstantsSummary/ImageConstants.swift
@@ -0,0 +1,122 @@
+//
+//  ImageConstants.swift
+//  TheBookSummary
+//
+//  Created by Somya on 09/06/20.
+//  Copyright © 2020 Somya. All rights reserved.
+//
+
+import Foundation
+
+struct ImageName {
+    
+    static let alchemist = "alchemist"
+    
+    
+    static let fires = "littlefireseverywhere"
+    
+    static let david = "davidcopperfield"
+    
+    static let oliver = "olivertwist"
+    
+    static let light = "allthelightthatyoucannotsee"
+    
+    static let gone = "gonegirl"
+    
+    static let pride = "pride-and-prejudice"
+    
+    static let emma = "emma"
+    
+    static let wuthering = "wutheringheights"
+    
+    static let clarissa = "clarissa"
+    
+    static let treasure = "treasureisland"
+    
+    static let huckleberry = "finn"
+    
+    static let frank = "frank"
+    
+    static let three = "the-three-musketeers"
+    
+    static let robinson = "robinson"
+    
+    static let great = "greatexpectations"
+    
+    static let power = "powerofnow"
+    
+    static let rich = "richdad"
+    
+    static let subtle = "subtleart"
+    
+    static let becoming = "becoming"
+    
+    static let ikigai = "ikigai"
+    
+    static let crawdad = "crawdads"
+    
+    static let search = "search"
+    
+    static let atomic = "atomichabits"
+    
+    static let bird = "birdbox"
+    
+    static let orwell = "1984"
+    
+    static let lies = "biglittlelies"
+    
+    static let peril = "peril"
+    
+    static let orient = "orient"
+    
+    static let silkworm = "silkworm"
+    
+    static let cuckoo = "cuckoo"
+    
+    static let nile = "nile"
+    
+    static let cards = "cards"
+    
+    static let sleeping = "sleeping"
+    
+    static let christmas = "christmas"
+    
+    static let none = "andtherewerenone"
+    
+    static let water = "water"
+    
+    static let inferno = "inferno"
+    
+    static let kite = "thekiterunner"
+    
+    static let mountains = "mountains"
+    
+    static let anne = "annefrank"
+    
+    static let steve = "stevejobs "
+    
+    static let pearl = "pearl"
+    
+    static let malala = "malala"
+    
+    static let elon = "elonmusk"
+    
+    static let wings = "wingsoffire "
+    
+    static let mind = "mind"
+    
+    static let wild = "wild "
+    
+    static let little = "littlewoman1"
+    
+    static let jones = "tom"
+    
+    static let mocking = "mockingbird"
+    
+    static let lowland = "thelowland"
+    
+    static let veronica = "veronica"
+    
+    static let hounds = "hounds"
+    
+}
---
+++ TheBookSummary/Utils/Helpers/ConvertToCategories.swift
@@ -0,0 +1,24 @@
+//
+//  ConvertToCategories.swift
+//  TheBookSummary
+//
+//  Created by Somya on 09/06/20.
+//  Copyright © 2020 Somya. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Converter {
+    
+    static func getListOfCategories(books: [FavouriteBook]) -> [Categories] {
+        
+        var listOfItems:[Categories] = []
+        var title:String
+        for book in books {
+            
+        }
+        
+    }
+    
+}
